bf95dc7124c9b4560c273024e3babb23b40a109f
Akshay <akshay.igenerate@gmail.com>
2024-09-17 16:18:21 +0530

    changes in login api

--- controllers/userController.js
+++ controllers/userController.js
@@ -109,7 +109,10 @@ export const loginUser = async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    res.json({ token });
+    res.status(200).json({
+      message: "Login successful",
+      token: token,
+    });
   } catch (error) {
     res.status(500).json({ error: error.message || "Server error" });
   }
